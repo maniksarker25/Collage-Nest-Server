5dff5dac1db0e890ff8d8ed642b181391d9e1913
Manik <tonysins43@gmail.com>
2023-07-22 13:59:04 +0600

    send spacific collage data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
@@ -32,6 +32,12 @@ async function run() {
 
     app.get('/collages', async(req,res)=>{
         const result = await collageCollection.find().toArray();
+        res.send(result);
+    })
+    app.get('/collages/:id', async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await collageCollection.findOne(query);
         res.send(result);
     })
 
